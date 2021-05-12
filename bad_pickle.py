16d4e67a122d85e2d8a3cc2357c0020c19152307
Richard Jandušík <xjandus@fi.muni.cz>
2021-05-12 14:55:02 +0200

    py c3

--- bad_pickle.py
+++ bad_pickle.py
@@ -3,12 +3,11 @@
 # https://hackernoon.com/10-common-security-gotchas-in-python-and-how-to-avoid-them-e19fbe265e03
 import subprocess
 import base64
-import docstring
 import cPickle
 
 
 # Input injection
-def transcode_file(request, filename):
+def transcode_file(filename):
     command = 'ffmpeg -i "{source}" output_file.mpg'.format(source=filename)
     subprocess.call(command, shell=True)  # a bad idea!
 
